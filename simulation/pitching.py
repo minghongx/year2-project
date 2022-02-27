d53886c9afae65166054fdbe1bac2c268637a353
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-02-27 23:35:23 +0000

    Improve the comments in pitching.py

--- simulation/pitching.py
+++ simulation/pitching.py
@@ -7,7 +7,7 @@ from time import sleep
 # Initialisation
 physics_server_id = bullet.connect(bullet.GUI)
 bullet.setRealTimeSimulation(enableRealTimeSimulation=True, physicsClientId=physics_server_id)
-bullet.setGravity(0, 0, -30, physics_server_id)  # FIXME 模型质量太轻
+bullet.setGravity(0, 0, -30, physics_server_id)  # FIXME: The model is too light
 import pybullet_data; bullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 bullet.loadURDF("plane.urdf", physicsClientId=physics_server_id)
 a1 = A1(physics_server_id)
@@ -24,33 +24,33 @@ debug_pitch_angle = bullet.addUserDebugParameter(
     rangeMax =  0.31,
     startValue = 0,)
 
-sleep(1)  # 初始化是异步的。等待一秒保证初始化结束再读取电机位置
+sleep(1)  # The initialisation is asynchronous. Before reading their position, wait one second to ensure that the motors reach their initial position
 ref_motor_positions = a1.motor_indices.applymap(lambda index: bullet.getJointState(a1.id, index, a1.in_physics_client)[0])
 while True:
-    motor_positions = ref_motor_positions.copy()  # 以 ref pos 为参照计算俯仰角
-    pitch_angle = bullet.readUserDebugParameter(debug_pitch_angle)  # 前为 0, 顺时针为正
+    motor_positions = ref_motor_positions.copy()  # The following calculation is with reference to the initial position of the motor, not the current position
+    pitch_angle = bullet.readUserDebugParameter(debug_pitch_angle)
 
     for leg, positions in motor_positions.items():
-        # 简写以增加可读性
-        t0, t1, t2 = positions
-        l1 = a1.thigh_len
-        l2 = a1.calf_len
+        # Abbreviating to increase readability
+        θ0, θ1, θ2 = positions
+        ℓ1 = a1.thigh_len
+        ℓ2 = a1.calf_len
         L  = a1.body_len / 2
         o  = a1.hip_offset
         δ  = pitch_angle
 
-        # 从当前电机位置得出当前足端相对于髋关节的位置
-        x = -l1 * np.sin(t1) - l2 * np.sin(t1 + t2)
-        h =  l1 * np.cos(t1) + l2 * np.cos(t1 + t2)
+        # θ0, θ1, θ2, ℓ1, ℓ2 ⇒ x, h, y, z
+        x = -ℓ1 * np.sin(θ1) - ℓ2 * np.sin(θ1 + θ2)
+        h =  ℓ1 * np.cos(θ1) + ℓ2 * np.cos(θ1 + θ2)
         match leg:
             case "fr" | "hr":
-                y = -o * np.cos(t0) - h * np.sin(t0)
-                z =  o * np.sin(t0) - h * np.cos(t0)
+                y = -o * np.cos(θ0) - h * np.sin(θ0)
+                z =  o * np.sin(θ0) - h * np.cos(θ0)
             case "fl" | "hl":
-                y =  o * np.cos(t0) - h * np.sin(t0)
-                z = -o * np.sin(t0) - h * np.cos(t0)
+                y =  o * np.cos(θ0) - h * np.sin(θ0)
+                z = -o * np.sin(θ0) - h * np.cos(θ0)
 
-        # pitch 后足端相对于髋关节的位置
+        # New x, z, h after pitching
         match leg:
             case "fr" | "fl":
                 pitch = np.array([[ np.cos(δ), -np.sin(δ),  L * np.cos(δ) - L ],
@@ -63,11 +63,11 @@ while True:
         x, z, _ = pitch.dot(np.array([x, z, 1]))
         h = np.sqrt(z**2 + y**2 - o**2)
 
-        # 从 pitch 后足端相对于髋关节的位置反算电机位置
-        c2 = (-l1**2 - l2**2 + x**2 + h**2) / (2 * l1 * l2)
-        s2 = -np.sqrt(1 - c2**2)  # sin 取负是因为模型中的 knee position 规定为负
-        positions[2] = np.arctan2(s2, c2)
-        positions[1] = np.arccos((l1**2 + x**2 + h**2 - l2**2) / (2 * l1 * np.sqrt(x**2 + h**2))) - np.arctan2(x, h)
+        # x, h, y, z ⇒ θ2, θ1
+        cosθ2 = (-ℓ1**2 - ℓ2**2 + x**2 + h**2) / (2 * ℓ1 * ℓ2)
+        sinθ2 = -np.sqrt(1 - cosθ2**2)  # takes negative suqare root, because the knee's position is specified as negative
+        positions[2] = np.arctan2(sinθ2, cosθ2)
+        positions[1] = np.arccos((ℓ1**2 + x**2 + h**2 - ℓ2**2) / (2 * ℓ1 * np.sqrt(x**2 + h**2))) - np.arctan2(x, h)
 
     # Set motor positions
     for positions, indices in zip(motor_positions.itertuples(index=False), a1.motor_indices.itertuples(index=False)):
