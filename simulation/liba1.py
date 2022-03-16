c167a14bd71c90c68af127b812ef89464bf16360
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-03-16 07:42:53 +0000

    Encapsulate pitching algorithm into liba1.py
    In addition, the demo for pitching has been simplified.

--- simulation/liba1.py
+++ simulation/liba1.py
@@ -27,20 +27,17 @@ class A1:
                              [  4 ,   9 ,  14 ,  19 ],]),
             index = ["hip abd/add", "hip fle/ext", "knee"])
         # A matrix contains motor information. The info is structured as
-        # Tuple(jointIndex, jointName, jointType, qIndex, uIndex, flags, jointDamping
-        # , jointFriction, jointLowerLimit, jointUpperLimit, jointMaxForce, jointMaxVelocity
-        # , linkName, jointAxis, parentFramePos, parentFrameOrn, parentIndex)
+        # Tuple(jointIndex, jointName, jointType, qIndex, uIndex, flags, jointDamping,
+        # jointFriction, jointLowerLimit, jointUpperLimit, jointMaxForce, jointMaxVelocity,
+        # linkName, jointAxis, parentFramePos, parentFrameOrn, parentIndex)
         # See PyBullet document for getJointInfo().
         self.motor_info = self.motor_indices.applymap(lambda index: bullet.getJointInfo(self.id, index))
         # Obtained by measuring the STL file of the thigh and calf. Both are 0.2mm.
         self.thigh_len = 200
         self.calf_len  = 200
-        # FIXME: Obtain more accurate values
-        self.hip_offset = 80
-        # FIXME: Obtain more accurate values
-        self.body_len = 360
-        # FIXME: Obtain more accurate values
-        self.body_width = 200
+        self.hip_offset = 80   # FIXME: Obtain more accurate value
+        self.body_len = 360    # FIXME: Obtain more accurate value
+        self.body_width = 200  # FIXME: Obtain more accurate value
         # Set initial postion of motors.
         bullet.setJointMotorControlArray(
             physicsClientId = self.in_physics_client,
@@ -60,3 +57,65 @@ class A1:
             jointIndices = self.motor_indices.loc["knee", :],
             controlMode = bullet.POSITION_CONTROL,
             targetPositions = np.full(4, -0.7*2),)  # ensures that the toes are beneath the hips
+
+
+    def current_motor_angular_positions(self):
+        return self.motor_indices.applymap(lambda index: bullet.getJointState(self.id, index, self.in_physics_client)[0])
+
+
+    def pitching(self, angle, ref_motor_angular_positions=None) -> None:
+
+        if ref_motor_angular_positions is None:
+            # FIXME: 取 angle 为 0.0 相对当前位置反复计算可以观察到, 误差会累加, 机器狗姿态慢慢畸形.
+            # Possible Sol: 数值精度缩小到 3 decimal places
+            angular_positions = self.current_motor_angular_positions()
+        else:
+            angular_positions = ref_motor_angular_positions.copy()
+
+        for leg, θ in angular_positions.items():  #! 引用传递; 修改 θ 即修改 angular_positions 中的元素
+            θ0, θ1, θ2 = θ  # θ is a tuple of three motor angular positions of one leg
+            # Abbreviating others to increase readability further
+            δ  = angle
+            ℓ1 = self.thigh_len
+            ℓ2 = self.calf_len
+            L  = self.body_len / 2
+            o  = self.hip_offset
+
+            # θ0, θ1, θ2 ⇒ x, h, y, z
+            x = -ℓ1 * np.sin(θ1) - ℓ2 * np.sin(θ1 + θ2)
+            h =  ℓ1 * np.cos(θ1) + ℓ2 * np.cos(θ1 + θ2)
+            match leg:
+                case "fr" | "hr":
+                    y = -o * np.cos(θ0) - h * np.sin(θ0)
+                    z =  o * np.sin(θ0) - h * np.cos(θ0)
+                case "fl" | "hl":
+                    y =  o * np.cos(θ0) - h * np.sin(θ0)
+                    z = -o * np.sin(θ0) - h * np.cos(θ0)
+
+            # x, z, h after pitching
+            match leg:
+                case "fr" | "fl":
+                    pitch = np.array([[ np.cos(δ), -np.sin(δ),  L * np.cos(δ) - L ],
+                                      [ np.sin(δ),  np.cos(δ),  L * np.sin(δ)     ],
+                                      [ 0,          0,          1                 ]])
+                case "hr" | "hl":
+                    pitch = np.array([[ np.cos(δ), -np.sin(δ), -L * np.cos(δ) + L ],
+                                      [ np.sin(δ),  np.cos(δ), -L * np.sin(δ)     ],
+                                      [ 0,          0,          1                 ]])
+            x, z, _ = pitch.dot(np.array([x, z, 1]))
+            h = np.sqrt(z**2 + y**2 - o**2)
+
+            # x, h, y, z ⇒ θ1, θ2
+            cosθ2 = (-ℓ1**2 - ℓ2**2 + x**2 + h**2) / (2 * ℓ1 * ℓ2)
+            sinθ2 = -np.sqrt(1 - cosθ2**2)  # takes negative suqare root, because the knee's position is specified as negative
+            θ[1] = np.arccos((ℓ1**2 + x**2 + h**2 - ℓ2**2) / (2 * ℓ1 * np.sqrt(x**2 + h**2))) - np.arctan2(x, h)
+            θ[2] = np.arctan2(sinθ2, cosθ2)
+
+        # Motor angular position control
+        for positions_of_one_leg, indices_of_one_leg in zip(angular_positions.itertuples(index=False), self.motor_indices.itertuples(index=False)):
+            bullet.setJointMotorControlArray(
+                physicsClientId = self.in_physics_client,
+                bodyUniqueId = self.id,
+                controlMode = bullet.POSITION_CONTROL,
+                jointIndices = indices_of_one_leg,
+                targetPositions = positions_of_one_leg,)
--- simulation/pitching.py
+++ simulation/pitching.py
@@ -1,4 +1,3 @@
-from cgitb import reset
 import pybullet as bullet
 import numpy as np
 from liba1 import A1
@@ -24,56 +23,8 @@ debug_pitch_angle = bullet.addUserDebugParameter(
     rangeMax =  0.31,
     startValue = 0,)
 
-sleep(1)  # The initialisation is asynchronous. Before reading their position, wait one second to ensure that the motors reach their initial position
-ref_motor_positions = a1.motor_indices.applymap(lambda index: bullet.getJointState(a1.id, index, a1.in_physics_client)[0])
+sleep(1)  # Ugly so FIXME
+# The initialisation is asynchronous. Wait one second to ensure that the motors reach their initial position before reading the position values.
+ref_pos = a1.current_motor_angular_positions()
 while True:
-    motor_positions = ref_motor_positions.copy()  # The following calculation is with reference to the initial position of the motor, not the current position
-    pitch_angle = bullet.readUserDebugParameter(debug_pitch_angle)
-
-    for leg, positions in motor_positions.items():
-        # Abbreviating to increase readability
-        θ0, θ1, θ2 = positions
-        ℓ1 = a1.thigh_len
-        ℓ2 = a1.calf_len
-        L  = a1.body_len / 2
-        o  = a1.hip_offset
-        δ  = pitch_angle
-
-        # θ0, θ1, θ2, ℓ1, ℓ2 ⇒ x, h, y, z
-        x = -ℓ1 * np.sin(θ1) - ℓ2 * np.sin(θ1 + θ2)
-        h =  ℓ1 * np.cos(θ1) + ℓ2 * np.cos(θ1 + θ2)
-        match leg:
-            case "fr" | "hr":
-                y = -o * np.cos(θ0) - h * np.sin(θ0)
-                z =  o * np.sin(θ0) - h * np.cos(θ0)
-            case "fl" | "hl":
-                y =  o * np.cos(θ0) - h * np.sin(θ0)
-                z = -o * np.sin(θ0) - h * np.cos(θ0)
-
-        # New x, z, h after pitching
-        match leg:
-            case "fr" | "fl":
-                pitch = np.array([[ np.cos(δ), -np.sin(δ),  L * np.cos(δ) - L ],
-                                  [ np.sin(δ),  np.cos(δ),  L * np.sin(δ)     ],
-                                  [ 0,          0,          1                 ]])
-            case "hr" | "hl":
-                pitch = np.array([[ np.cos(δ), -np.sin(δ), -L * np.cos(δ) + L ],
-                                  [ np.sin(δ),  np.cos(δ), -L * np.sin(δ)     ],
-                                  [ 0,          0,          1                 ]])
-        x, z, _ = pitch.dot(np.array([x, z, 1]))
-        h = np.sqrt(z**2 + y**2 - o**2)
-
-        # x, h, y, z ⇒ θ2, θ1
-        cosθ2 = (-ℓ1**2 - ℓ2**2 + x**2 + h**2) / (2 * ℓ1 * ℓ2)
-        sinθ2 = -np.sqrt(1 - cosθ2**2)  # takes negative suqare root, because the knee's position is specified as negative
-        positions[2] = np.arctan2(sinθ2, cosθ2)
-        positions[1] = np.arccos((ℓ1**2 + x**2 + h**2 - ℓ2**2) / (2 * ℓ1 * np.sqrt(x**2 + h**2))) - np.arctan2(x, h)
-
-    # Set motor positions
-    for positions, indices in zip(motor_positions.itertuples(index=False), a1.motor_indices.itertuples(index=False)):
-        bullet.setJointMotorControlArray(
-            physicsClientId = physics_server_id,
-            bodyUniqueId = a1.id,
-            jointIndices = indices,
-            controlMode = bullet.POSITION_CONTROL,
-            targetPositions = positions,)
+    a1.pitching(bullet.readUserDebugParameter(debug_pitch_angle), ref_pos)
